a7e48d3a6c363b47f931065166bb458055e6f5b7
kanna201010 <robertstalin0@gmail.com>
2025-07-16 14:24:26 +0530

    Create  arbLogic.js

---
+++ arbLogic.js
@@ -0,0 +1,8 @@
+module.exports.buildArbTransaction = async function () {
+  // This is a placeholder to test deployment
+  return {
+    to: "0x000000000000000000000000000000000000dead",
+    data: "0x",
+    value: 0
+  };
+};
