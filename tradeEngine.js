e31f767be6b4bc4ddea3b875e3c596c69830e9eb
kanna201010 <robertstalin0@gmail.com>
2025-07-16 14:42:57 +0530

    Update tradeEngine.js

--- tradeEngine.js
+++ tradeEngine.js
@@ -1,16 +1,15 @@
-const { ethers } = require("ethers");
+const ethers = require("ethers");
 const { buildArbTransaction } = require('./arbLogic');
-require('dotenv').config();
 
 module.exports = async function () {
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-  const txData = await buildArbTransaction(); // Your custom calldata builder
+  const txData = await buildArbTransaction(); // Returns: { to, data, value }
 
   const gasSettings = {
-    maxFeePerGas: ethers.utils.parseUnits("100", "gwei"),
-    maxPriorityFeePerGas: ethers.utils.parseUnits("50", "gwei"),
+    maxFeePerGas: ethers.parseUnits("100", "gwei"),
+    maxPriorityFeePerGas: ethers.parseUnits("50", "gwei"),
     gasLimit: 700000
   };
 
